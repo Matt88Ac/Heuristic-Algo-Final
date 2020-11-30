771ff69781d565ec49361344063bcafac9fe65be
Matt8ac <Matt8ac@gmail.com>
2020-11-30 16:41:48 +0200

    Update RoadMap.py

--- RoadMap.py
+++ RoadMap.py
@@ -2,13 +2,12 @@ import folium
 import webbrowser
 import os
 from selenium import webdriver
-import html
 from datetime import datetime
 
 
 class RoadMap:
 
-    def __init__(self, start: tuple, loc_name: str, dest: tuple, dest_name: str, zoom=15.5):
+    def __init__(self, start: tuple, loc_name: str, dest: tuple, dest_name: str, zoom=14.5):
         self.display_page = 'map.html'
         self.destination_tooltip = 'From'
         self.map = folium.Map(location=start, tiles='stamenwatercolor', zoom_start=zoom)
@@ -19,16 +18,19 @@ class RoadMap:
         self.fname = self.dir + '/' + self.display_page
         os.mkdir(self.dir)
 
-        folium.Marker(start, popup='<i>' + loc_name + '</i>', tooltip=self.destination_tooltip).add_to(self.map)
-        folium.Marker(dest, popup='<i>' + dest_name + '</i>', tooltip='To').add_to(self.map)
+        self.map.save(self.fname)
+        self.driver = webdriver.Chrome()
+        self.driver.get('file://' + self.fname)
+        self.driver.save_screenshot(self.dir + '/out.png')
+
+        folium.Marker(start, popup='<i>' + loc_name + '</i>', tooltip=self.destination_tooltip,
+                      icon=folium.Icon(color='green')).add_to(self.map)
+        folium.Marker(dest, popup='<i>' + dest_name + '</i>', tooltip='To', icon=folium.Icon(color='red')
+                      ).add_to(self.map)
 
         self.map.save(self.fname)
         self.start = start
         self.end = dest
-
-        self.driver = webdriver.Chrome()
-        self.driver.get('file://' + self.fname)
-        self.driver.save_screenshot(self.dir + '/out.png')
 
 
 rr = RoadMap((32.014191794417144, 34.773603467664515), 'hit', (32.016391794417145, 34.773603467664515), 'muir')
