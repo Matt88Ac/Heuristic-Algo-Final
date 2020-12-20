ff38de092e0162ba83c0a58c53a3b6e9e2bb46e8
Matt8ac <Matt8ac@gmail.com>
2020-12-20 11:08:43 +0200

    over to osmnx

--- Agent.py
+++ Agent.py
@@ -1,3 +1,24 @@
+import osmnx as ox
+import networkx as nx
+
+start = (32.0141, 34.7745)
+end = (32.0148, 34.7736)
 
 
+G = ox.graph_from_point((32.0141, 34.7736), dist=dist, network_type='walk')
 
+start_node = ox.get_nearest_node(G, start)
+end_node = ox.get_nearest_node(G, end)
+G.add_node(start_node)
+G.add_node(end_node)
+
+#ox.plot_graph(G, node_color='blue', bgcolor='white', edge_color='k')
+
+
+route2 = nx.shortest_path(G, start_node, end_node, weight='travel_time')
+print(route2)
+#route3 = nx.shortest_path(G, start_node[0], end_node, weight='travel_time')
+
+#ox.plot_graph_route(G, route1, route_linewidth=6, node_size=0, bgcolor='k')
+##ox.plot_graph_route(G, route2, route_linewidth=6, node_size=0, bgcolor='k')
+#ox.plot_graph_route(G, route3, route_linewidth=6, node_size=0, bgcolor='k')
--- RoadMap.py
+++ RoadMap.py
@@ -1,5 +1,5 @@
-import folium
-import webbrowser
+import osmnx as ox
+import networkx as nx
 import os
 from selenium import webdriver
 from datetime import datetime
@@ -8,70 +8,16 @@ import numpy as np
 
 class RoadMap:
 
-    def __init__(self, start: tuple, loc_name: str, dest: tuple, dest_name: str, zoom=14.5):
-        self.display_page = 'map.html'
-        self.destination_tooltip = 'From'
-        x0, y0 = start
-        x1, y1 = dest
+    def __init__(self, start: tuple, end: tuple, network_type='walk'):
+        dist = 900000 * ((start[0] - end[0]) ** 2 + (start[1] - end[1]) ** 2) ** 0.5
+        self.dist = dist
+        self.st = start
+        self.end = end
+        self.G = ox.graph_from_point((32.0141, 34.7736), dist=dist, network_type=network_type)
 
-        self.map = folium.Map(location=start, tiles='stamenwatercolor')
-        self.map.add_child(folium.LatLngPopup())
-
-        now = datetime.now().strftime("%d-%m-%Y%H-%M-%S")
-        curdir = os.getcwd().replace(os.getcwd()[2], '/') + '/Maps'
-        if not os.path.exists(curdir):
-            os.makedirs(curdir)
-        self.dir = curdir + '/' + now
-        self.fname = self.dir + '/' + self.display_page
-        os.mkdir(self.dir)
-
-        self.map.save(self.fname)
-        self.driver = webdriver.Chrome()
-        self.driver.get('file://' + self.fname)
-        self.driver.save_screenshot(self.dir + '/out.png')
-
-        folium.Marker(start, popup='<i>' + loc_name + '</i>', tooltip=self.destination_tooltip,
-                      icon=folium.Icon(color='green')).add_to(self.map)
-        folium.Marker(dest, popup='<i>' + dest_name + '</i>', tooltip='To', icon=folium.Icon(color='red')
-                      ).add_to(self.map)
-
-        self.map.save(self.fname)
-        self.start = start
-        self.end = dest
-
-        self.zoom = zoom
-
-    def get_indexes_of_square_by_coordinate(self, c):  # each square is cell in the matrix
-        x = 1
-        y = 1
-        # calculate the picture width and high
-        # take another coordinate in the range and ???
-        return x, y
-
-    def get_coordinate_by_indexes_of_square(self, x, y):  # each square is cell in the matrix
-        c = 1
-        return c
-
-    def xyToPixels(self) -> tuple:
-        x0, y0 = self.start
-        x1, y1 = self.end
-
-        def corToPix(x, y):
-            scale = 2 ** self.zoom
-
-            t0 = np.radians(x)
-            t1 = np.radians(y)
-
-            t0 = ((t0 + 180) / 360) * scale
-            t1 = (1 - (np.log(np.tan(t1) + 1.0 / np.cos(t1)) / np.pi)) * scale / 2.0
-
-            return [np.floor(t0), np.floor(t1)]
-
-        return corToPix(x0, y0), corToPix(x1, y1)
-
-    def loadPage(self):
-        webbrowser.open(self.fname)
+    def plot(self):
+        ox.plot_graph(self.G, node_color='blue', bgcolor='white', edge_color='k')
 
 
-rr = RoadMap((32.0141, 34.7736), 'hit', (32.0163, 34.7736), 'muir')
-print(rr.zoom)
+rr = RoadMap((32.0141, 34.7736), (32.0163, 34.7736))
+rr.plot()
