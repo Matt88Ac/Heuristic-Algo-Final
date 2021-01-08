c505206433a54abfd1be926403ebe7bddfc697ca
Matt8ac <Matt8ac@gmail.com>
2021-01-08 13:50:11 +0200

    Update Stats_analysis.py

--- Stats_analysis.py
+++ Stats_analysis.py
@@ -8,26 +8,27 @@ style.use('ggplot')
 
 data = pd.read_csv('our_data.csv')
 names_metric = ['Sphere', 'Euclidean', 'Manhattan', 'Octile', 'Chebyshev']
-cols = ['Sphere Distance', 'Time(s)', 'Number of Steps', 'Algorithm', 'Metric Space',
+cols = ['Sphere Distance', 'Time(s)', 'Number of Steps', 'Algorithm', 'Metric Space', 'Number of Vertex',
         'Total Path Weight', 'Network Type']
 
 # data = data.loc[data['Metric Space'] != 'H(x, y)=0', :]
 
 points = np.unique(data['Sphere Distance'])
-#xx = []
-#for p in points:
-#    for way in ['drive', 'bike']:
-#        to_calc = data.loc[(data['Sphere Distance'] == p) & (data['Network Type'] == way), :]
-#        w = to_calc.loc[to_calc['Algorithm'] != 'A*', 'Total Path Weight'].to_numpy(dtype=float)[0]
-#        to_calc = np.abs(to_calc.loc[:, 'Total Path Weight'].to_numpy(dtype=float) - w)
-#        xx.extend(to_calc.tolist())
-#
-#data['Precision'] = xx
+xx = []
+for p in points:
+   for way in ['drive', 'bike']:
+       to_calc = data.loc[(data['Sphere Distance'] == p) & (data['Network Type'] == way), :]
+       w = to_calc.loc[to_calc['Algorithm'] != 'A*', 'Total Path Weight'].to_numpy(dtype=float)[0]
+       to_calc = np.abs(to_calc.loc[:, 'Total Path Weight'].to_numpy(dtype=float) - w)
+       xx.extend(to_calc.tolist())
 
-#plt.ylabel('Error')
-#plt.xlabel('$d(p_1, p_2)$')
-#plt.title('Error: Comparing between heuristic functions for $A^*$')
+data['Precision'] = xx
 
-# data = data.loc[data['Algorithm'] != 'Dijkstra']
-#sns.catplot(x='Metric Space', y='Number of Steps', data=data, col='Network Type', hue='Sphere Distance', kind='bar')
+plt.ylabel('Error')
+plt.xlabel('$|V|$')
+plt.title('Mean of $A^*$s Total Error')
+data = data.loc[data['Algorithm'] != 'Dijkstra']
 
+sns.lineplot(x='Number of Vertex', y='Precision', data=data, hue='Algorithm', style='Network Type')
+plt.show()
+# sns.catplot(x='Metric Space', y='Number of Steps', data=data, col='Network Type', hue='Sphere Distance', kind='bar')
